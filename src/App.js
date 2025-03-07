73c6a180b129da06f770caaa8f074f6e3d2191d7
DEEP NARAYAN MAURYA <deepmaurya025@gmail.com>
2025-03-07 15:16:24 +0530

    Changes routes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { ErrorBoundary } from "react-error-boundary";
-import ErrorFallback from "./components/ErrorFallback";
+
 
 function App() {
   return (
     <Router>
       <Header />
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
         <Routes>
-          <Route path="/" element={<Home />} />
+           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </ErrorBoundary>
       <Footer />
     </Router>
   );
